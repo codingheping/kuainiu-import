f2e32288a96ffeacffe0bef680de2d6879d510d7
he ping <847050412@qq.com>
2021-02-05 17:12:38 +0800

    更改语言包路径

--- src/Module.php
+++ src/Module.php
@@ -39,7 +39,7 @@ class Module extends \yii\base\Module implements BootstrapInterface
             Yii::$app->i18n->translations['import'] = [
                 'class'            => PhpMessageSource::class,
                 'forceTranslation' => true,
-                'basePath'         => '@vendor/kuainiu/import/src/messages',
+                'basePath'         => '@vendor/codingheping/import/src/messages',
             ];
         }
         $event = new Event();
